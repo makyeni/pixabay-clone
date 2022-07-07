b144280da38ac69ba06e3d5508a1b7659fe04238
MAKYENI <52960601+makyeni@users.noreply.github.com>
2022-07-07 15:33:11 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <nav class="bg-white-700 border-red-200 px-2 sm:px-4 py-2.5  p-14 rounded dark:bg-gray-800">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
-  <a href="https://flowbite.com/" class="flex  ml-11">
+  <a href="" class="flex  ml-11">
       
       <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PEXELS</span>
   </a>
@@ -40,7 +40,7 @@ License</a>
 
 <div v-for="(image,i) in images" :key="i"  class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
     </a>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ image.user }}</p>
     <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
